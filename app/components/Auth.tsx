0a7605548f30d583c6786edd3664bda3075e6433
Astrofic 7 <astrofic7@gmail.com>
2024-08-04 21:11:15 +0545

    auth page ui

--- app/components/Auth.tsx
+++ app/components/Auth.tsx
@@ -72,67 +72,81 @@ const Auth = () => {
 
   return (
     <div className="login-container bg-gray-800/70 h-[90vh] w-[90vw] flex">
-      <div className="login-leftside-container">
-        <Image
-          height={200}
-          width={200}
-          className="logo"
-          src="/images/Chater.png"
-          alt="Logo"
-        />
-      </div>
-      <div className="login-rightside-container">
-        <h2 className="font-bold">
+      <div className="flex flex-1 bg-gradient-to-br from-orange-200 to-orange-500 flex-col justify-center items-center">
+        <h2 className="font-bold text-4xl mb-2">
           {!isSignUp ? "Register your Account" : "Login to your Account"}
         </h2>
-        {!isSignUp && (
-          <>
-            <TextField
-              onChange={changeHandler}
-              name="name"
-              value={data.name}
-              id="outlined-basic"
-              label="Username"
-              variant="outlined"
-            />
-            <br />
-          </>
-        )}
+        <p className="text-center text-sm">
+          Register or login to access more information.
+        </p>
+        <div className="flex mt-5 flex-col border-2 border-orange-200/70 p-5 rounded-md">
+          {!isSignUp && (
+            <>
+              <label
+                htmlFor="name"
+                className="text-sm font-semibold text-orange-600"
+              >
+                Name
+              </label>
+              <input
+                onChange={changeHandler}
+                name="name"
+                className="bg-orange-200 outline-none rounded-sm p-2"
+                placeholder="Username"
+                type="text"
+                value={data.name}
+                id="name"
+              />
+              <br />
+            </>
+          )}
+          <label
+            htmlFor="name"
+            className="text-sm font-semibold text-orange-600"
+          >
+            Email
+          </label>
+          <input
+            onChange={changeHandler}
+            name="email"
+            type="text"
+            className="bg-orange-200 outline-none rounded-sm p-2"
+            placeholder="Email"
+            value={data.email}
+            id="email"
+          />
+          <br />
+          <label
+            htmlFor="name"
+            className="text-sm font-semibold text-orange-600"
+          >
+            Password
+          </label>
+          <input
+            onChange={changeHandler}
+            name="password"
+            value={data.password}
+            id="password"
+            type="password"
+            className="bg-orange-200 outline-none rounded-sm p-2"
+            placeholder="Password"
+            autoComplete="current-password"
+          />
+          <br />
+          <button
+            onClick={isSignUp ? loginHandler : signUpHandler}
+            id="login-button"
+            className="font-bold bg-blue-600 p-2 mb-2 text-white rounded-md"
+          >
+            {isSignUp ? "LOGIN" : "SIGN UP"}
+          </button>
+        </div>
 
-        <TextField
-          onChange={changeHandler}
-          name="email"
-          value={data.email}
-          id="outlined-basic"
-          label="Email"
-          variant="outlined"
-        />
-        <br />
-
-        <TextField
-          onChange={changeHandler}
-          name="password"
-          value={data.password}
-          id="outlined-basic"
-          label="Password"
-          type="password"
-          variant="outlined"
-          autoComplete="current-password"
-        />
-        <br />
-        <Button
-          onClick={isSignUp ? loginHandler : signUpHandler}
-          id="login-button"
-          variant="contained"
-          className="font-bold"
-        >
-          {isSignUp ? "LOGIN" : "SIGN UP"}
-        </Button>
-        <span className="font-bold text-black text-sm">
+        <span className="font-bold text-orange-900 text-sm mt-2">
           {!isSignUp ? "Already have an Account? " : "Don't have an Account? "}
           <a
             href="#"
-            className="font-bold text-black text-sm underline"
+            className="font-bold text-sm underline text-rose-700"
             onClick={handleToggle}
           >
             {" "}
@@ -145,3 +159,74 @@ const Auth = () => {
 };
 
 export default Auth;
+
+// <div className="login-container bg-gray-800/70 h-[90vh] w-[90vw] flex">
+//   <div className="login-leftside-container">
+//     <Image
+//       height={200}
+//       width={200}
+//       className="logo"
+//       src="/images/Chater.png"
+//       alt="Logo"
+//     />
+//   </div>
+//   <div className="login-rightside-container">
+//     <h2 className="font-bold">
+//       {!isSignUp ? "Register your Account" : "Login to your Account"}
+//     </h2>
+//     {!isSignUp && (
+//       <>
+//         <TextField
+//           onChange={changeHandler}
+//           name="name"
+//           value={data.name}
+//           id="outlined-basic"
+//           label="Username"
+//           variant="outlined"
+//         />
+//         <br />
+//       </>
+//     )}
+
+//     <TextField
+//       onChange={changeHandler}
+//       name="email"
+//       value={data.email}
+//       id="outlined-basic"
+//       label="Email"
+//       variant="outlined"
+//     />
+//     <br />
+
+//     <TextField
+//       onChange={changeHandler}
+//       name="password"
+//       value={data.password}
+//       id="outlined-basic"
+//       label="Password"
+//       type="password"
+//       variant="outlined"
+//       autoComplete="current-password"
+//     />
+//     <br />
+//     <Button
+//       onClick={isSignUp ? loginHandler : signUpHandler}
+//       id="login-button"
+//       variant="contained"
+//       className="font-bold"
+//     >
+//       {isSignUp ? "LOGIN" : "SIGN UP"}
+//     </Button>
+//     <span className="font-bold text-black text-sm">
+//       {!isSignUp ? "Already have an Account? " : "Don't have an Account? "}
+//       <a
+//         href="#"
+//         className="font-bold text-black text-sm underline"
+//         onClick={handleToggle}
+//       >
+//         {" "}
+//         {!isSignUp ? "Login" : "Sign Up"}
+//       </a>
+//     </span>
+//   </div>
+// </div>
